2be59ce8a4d0ab3d29447ddc32a4019f497bd0cf
Rune Lund-Søltoft <rune@lundsoltoft>
2021-05-25 15:03:19 +0200

    Rest provider

--- src/Server.fs
+++ src/Server.fs
@@ -15,7 +15,7 @@ let private appRouter = router {
     fetch <@ allConfigurations @>
     fetch <@ ping @>
     withArg <@ configuration @>
-    withArgs <@ meta @>
+    withArgs <@ meta @> 
     withArg <@ transformation @>
     withBody <@ storeConfiguration @>
     withBody <@ storeTransformation @>
